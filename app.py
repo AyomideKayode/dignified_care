8f02b95b351ddd168ba707df72be5eb98c6b6e81
Ayomide🤝🏽 <ayomidekay7@gmail.com>
2024-06-09 12:53:52 +0100

    Update with detailed comments for clarity.🚧

--- app.py
+++ app.py
@@ -39,26 +39,37 @@ def submit_form():
 
         # Create the email content
         email_content = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+        # Create a MIMEText object with the email content
         msg = MIMEText(email_content)
+        # Set the subject of the email
         msg['Subject'] = 'Dignified Care New Contact Form Submission'
+        # Set the sender of the email
         msg['From'] = f"{name} <{email}>"
+        # Set the recipient of the email
         msg['To'] = 'admin@dignifiedcare.uk'
 
         # Send the email
         try:
+            # Create an SMTP server object with the Gmail SMTP
+            #  server address and port number
             with smtplib.SMTP('smtp.gmail.com', 587) as server:
+                # Start a secure TLS connection
                 server.starttls()
+                # Login to the email account using the provided
+                # email address and password
                 server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+                # Send the email message
                 server.send_message(msg)
+            # Return a success response if the email is sent successfully
             return jsonify({"status": "success"}), 200
         except smtplib.SMTPException as e:
+            # Handle SMTP errors and return an error response
             print(f"SMTP error: {e}")
-            return jsonify(
-                {"status": "error", "message": f"SMTP error: {e}"}), 500
+            return jsonify({"status": "error", "message": f"SMTP error: {e}"}), 500
         except Exception as e:
+            # Handle general errors and return an error response
             print(f"General error: {e}")
-            return jsonify(
-                {"status": "error", "message": f"General error: {e}"}), 500
+            return jsonify({"status": "error", "message": f"General error: {e}"}), 500
 
     except Exception as e:
         print(f"Failed to process form data: {e}")
